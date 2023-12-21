e34349ef3ae796b48007ce134ed6dbcf0824cf27
admin <admin@MAC-STUDIO-JB.local>
2023-12-21 14:07:07 -0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+//samediff web v0.1.1
+
+import type { NextPage } from 'next'
+import Main from '../components/main1'
+const Home: NextPage = () => {
+  return (
+  <>
+      <Main />
+  </>
+  )
+}
+export default Home
