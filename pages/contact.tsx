3cf347f210a516dc96ea642f09533aadac2ebd4b
admin <admin@MAC-STUDIO-JB.local>
2024-01-22 06:08:20 -0800

    contact fade fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import ContactComponent from '../components/contact13'
+import ContactComponent from '../components/contact14'
 const Home: NextPage = () => {
   return (
   <>
